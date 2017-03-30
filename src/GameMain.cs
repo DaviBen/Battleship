4298861f903d675cbc169321c004311c39190f59
Maxxy1000 <101065819@student.swin.edu.au>
2017-03-31 10:28:34 +1100

    used xamarin template

---
+++ src/GameMain.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Linq;
+using System.Xml.Linq;
+namespace BattleShip
+{
+	static class GameMain
+	{
+		public static void Main()
+		{
+			//Opens a new Graphics Window
+			SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+
+			//Load Resources
+			GameResources.LoadResources();
+
+			SwinGame.PlayMusic(GameResources.GameMusic("Background"));
+
+			//Game Loop
+			do {
+				GameController.HandleUserInput();
+				GameController.DrawScreen();
+			} while (!(SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting));
+
+			SwinGame.StopMusic();
+
+			//Free Resources and Close Audio, to end the program.
+			GameResources.FreeResources();
+		}
+	}
+}
