2b09c5f15aade2a3ab94d40e8f30a41e469694fc
BENJAMIN <100614799@student.swin.edu.au>
2017-04-18 15:46:49 +1000

    Text entry for player name on highscore screen is now correct length

--- Battleship_CS_2017/src/HighScoreController.cs
+++ Battleship_CS_2017/src/HighScoreController.cs
@@ -178,7 +178,7 @@ static class HighScoreController
 			int x = 0;
 			x = SCORES_LEFT + SwinGame.TextWidth(GameResources.GameFont("Courier"), "Name: ");
 
-			SwinGame.StartReadingText(Color.White, NAME_WIDTH, GameResources.GameFont("Courier"), x, ENTRY_TOP);
+			SwinGame.StartReadingText(Color.White, NAME_WIDTH+1, GameResources.GameFont("Courier"), x, ENTRY_TOP);
 
 			//Read the text from the user
 			while (SwinGame.ReadingText()) {
