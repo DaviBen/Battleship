924939309ea4c751a0743051fb63831d8f34d61d
yashan047 <lamkwunshan@gmail.com>
2017-04-28 09:17:58 +1000

    Adding cheating button and game restart feature

--- Battleship_CS_2017/src/DiscoveryController.cs
+++ Battleship_CS_2017/src/DiscoveryController.cs
@@ -26,6 +26,11 @@ static class DiscoveryController
 			GameController.AddNewState(GameState.ViewingGameMenu);
 		}
 
+		if (SwinGame.KeyTyped(KeyCode.vk_q))
+		{
+			GameController.StartGame();
+		}
+
 		if (SwinGame.MouseClicked(MouseButton.LeftButton)) {
 			DoAttack();
 		}
@@ -63,8 +68,10 @@ static class DiscoveryController
 		const int HITS_TOP = 206;
 		const int SPLASH_TOP = 256;
 
-		if ((SwinGame.KeyDown(KeyCode.vk_LSHIFT) | SwinGame.KeyDown(KeyCode.vk_RSHIFT)) & SwinGame.KeyDown(KeyCode.vk_c)) {
+		//Cheat button	
+		if (SwinGame.KeyDown(KeyCode.vk_c)) {
 			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, true);
+
 		} else {
 			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, false);
 		}
